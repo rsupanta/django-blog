9dbb234b2b639f5142faf07b99e2a42c8baeacae
rsupanta <md.ragib.supanta@gmail.com>
2020-04-12 16:14:48 +0600

    added model for Post and Comment, Fixed TimeZone

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    create_time = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
