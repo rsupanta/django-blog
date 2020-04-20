110954f22f55d131ae7ba4131dead85462c00045
rsupanta <30446590+rsupanta@users.noreply.github.com>
2020-04-21 03:01:33 +0600

    initiating profile for user

--- django-blog/settings.py
+++ django-blog/settings.py
@@ -124,3 +124,4 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 
@@ -15,3 +16,8 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
