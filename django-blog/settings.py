c1e16f90168337cd3d607be9a028411ee55f1a10
rsupanta <30446590+rsupanta@users.noreply.github.com>
2020-04-21 14:48:42 +0600

    New model added for profile. Now it supports img

--- django-blog/settings.py
+++ django-blog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django-blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-blog-DB',
+        'USER': 'postgres',
+        'PASSWORD': 'rbt 7007',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -120,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = ''
+
+MEDIA_URL = ''
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
